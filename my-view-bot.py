ca10eef44c78a35e0d0b7b3bdbb11f7d361f945e
toniwidodo123 <litoninot12345@gmail.com>
2021-09-18 05:31:58 -0700

    change my view bot

--- my-view-bot.py
+++ my-view-bot.py
@@ -9,10 +9,13 @@ url = [
 		"https://www.youtube.com/watch?v=pZ1UTRE_jmA",
 		"https://www.youtube.com/watch?v=i2mF9DsjOZI",
 		"https://www.youtube.com/watch?v=jJyD6Md6Dfs",
+	],
+	[
 		"https://www.youtube.com/watch?v=tVDtj0hD7Ms"
-	],[
 		"https://www.youtube.com/watch?v=LurlNwa-uJo",
 		"https://www.youtube.com/watch?v=WuKolk-p3nQ",
+	],
+	[
 		"https://www.youtube.com/watch?v=TxICB8YGJmU",
 		"https://www.youtube.com/watch?v=NNWEqMXnP68",
 		"https://www.youtube.com/watch?v=8NRc6RuFtaw&t=41s"
@@ -31,7 +34,7 @@ for index, item in enumerate(url):
 		WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='Play (k)']"))).click()
 		time.sleep(5)
 	
-	time.sleep(180)
+	time.sleep(80)
 	driver.switch_to.window(driver.window_handles[1])
 	driver.execute_script("window.close()")
 	time.sleep(3)
